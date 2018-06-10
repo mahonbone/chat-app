5fbb8aadfa54064a24099f1314b65e797e1656b6
Sean Mahoney <mahonbone@gmail.com>
2018-06-10 09:11:55 -0500

    successfully added messaging feature. In this build messages go to the console.

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(rec, res){
     res.sendFile(__dirname + "/index.html");
 });
 
+io.on('connection', function(socket){
+    console.log('A user connected');
+    socket.on('disconnect', function(){
+        console.log('A user disconnected');
+    });
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+});
+
 http.listen(3000, function(){
-    console.log("listening on *:3000");
+    console.log('listening on *:3000');
 });
